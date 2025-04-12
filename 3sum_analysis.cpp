6bf37e6742feb53122b61462165fc5854ed96465
Arcticawesome <kaitkachuk@ucsb.edu>
2025-04-11 21:26:37 -0700

    done with optimized solution

--- 3sum_analysis.cpp
+++ 3sum_analysis.cpp
@@ -9,7 +9,7 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> result;
+        /*vector<vector<int>> result;
         int n = nums.size();
         sort(nums.begin(), nums.end());
         
@@ -29,6 +29,46 @@ public:
             }
         }
         return result;
+        */
+       vector<vector<int>> result;
+       int n = nums.size();
+       sort(nums.begin(), nums.end());
+       
+       for (int i = 0; i < n - 2; ++i) {
+           if (i > 0 && nums[i] == nums[i - 1]) {
+               continue;
+           }
+           
+           int p1 = i + 1;
+           int p2 = n - 1;
+           
+           while (p1 < p2) {
+               int sum = nums[i] + nums[p1] + nums[p2];
+               
+               if (sum < 0) {
+                   p1++;
+               } else if (sum > 0) {
+                   p2--;
+               } else {
+                   vector<int> triplet;
+                   triplet.push_back(nums[i]);
+                   triplet.push_back(nums[p1]);
+                   triplet.push_back(nums[p2]);
+                   result.push_back(triplet);
+                   
+                   while (p1 < p2 && nums[p1] == nums[p1 + 1]) {
+                       p1++;
+                   }
+                   while (p1 < p2 && nums[p2] == nums[p2 - 1]) {
+                       p2--;
+                   }
+                   
+                   p1++;
+                   p2--;
+               }
+           }
+       }
+       return result;
     }
 };
 
